f4867b9e507e984bf6c1ca5ac373991de1a3fc50
Felix Tripier <ftripier@gmail.com>
2013-08-14 21:50:10 -0700

    Update five.js

--- five.js
+++ five.js
@@ -5,7 +5,11 @@ Fivejs.five = function(num) {
     if (typeof num !== 'number') {
       throw "invalid input to FiveJs.five() " + num + ' is not a mnumber.';
     } else {
-      return (num*5/5 ) + 5 - 5;
+      var result;
+      for (var i = 1; i !== 5; i++){
+      result = (num*i/i ) + i - i;
+      return result
+      }
     }
   } catch (err) {
     console.log('Error: ' + err);
