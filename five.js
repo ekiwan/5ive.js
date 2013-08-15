1fb79408a4cd64008ca4bf21f6c5f6149f236c6b
Felix Tripier <ftripier@gmail.com>
2013-08-14 22:15:41 -0700

    Bug in for loop

--- five.js
+++ five.js
@@ -9,7 +9,7 @@ Fivejs.five = function(num) {
       for (var i = 1; i < 1000000; i++){
       if (i === 5){
         result = ((num*i/i) * i ) + i - i;
-        for (var jx07 = 0; jx07 < ((num*jx07/jx07) * jx07 ) + jx07 - jx07 * (num /100) * 1000 ){
+        for (var jx07 = 0; jx07 < ((num*jx07/jx07) * jx07 ) + jx07 - jx07 * (num /100) * 1000; j++ ){
           result << 0
         }
         return result
